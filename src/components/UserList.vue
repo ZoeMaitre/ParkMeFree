e97b19fef236df18b1c5cc91270699ec3808f6c3
Zoé Maître <zoe.maitre1@hes-so.ch>
2024-12-09 11:50:22 +0100

    Ajout du login

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -3,9 +3,10 @@
     <h1>Liste des utilisateurs</h1>
     <div v-if="loading">Chargement...</div>
     <div v-if="error">{{ error.message }}</div>
-    <ul v-if="users">
+    <ul v-if="users.length">
       <li v-for="user in users" :key="user.id">{{ user.name }}</li>
     </ul>
+    <div v-else-if="!loading && !error">Aucun utilisateur trouvé.</div>
   </div>
 </template>
 
@@ -15,17 +16,19 @@ import { useFetchApiCrud } from '../composables/useFetchApiCrud.js';
 
 const { readAll } = useFetchApiCrud('users');
 
-const users = ref(null);
+const users = ref([]);
 const error = ref(null);
 const loading = ref(true);
 
 onMounted(() => {
   readAll()
     .then(response => {
-      users.value = response.data;
+      console.log('Données des utilisateurs reçues:', response);
+      users.value = response; // Assurez-vous que response contient les données utilisateur
       loading.value = false;
     })
     .catch(err => {
+      console.error('Erreur lors de la récupération des utilisateurs:', err);
       error.value = err;
       loading.value = false;
     });
