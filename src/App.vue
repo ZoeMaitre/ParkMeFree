d6111999e09cf3f0df04a0032d5df9b354803200
Yonatan Teklezgi <yonatan.teklezgi@heig-vd.ch>
2024-12-09 12:01:54 +0100

    signup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue';
 import UserList from './components/UserList.vue';
+import SignUp from './components/SignUp.vue';
 
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -16,7 +17,8 @@ import UserList from './components/UserList.vue';
   <HelloWorld msg="Vite + Vue" />
   <div id="app">
     <UserList />
-  </div>
+  </div> -->
+  <SignUp />
 </template>
 
 <style scoped>
@@ -26,10 +28,47 @@ import UserList from './components/UserList.vue';
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Accueil</router-link><br>
+      <router-link to="/login">Connexion</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useFetchApiCrud } from './composables/useFetchApiCrud.js';
+
+const { readAll } = useFetchApiCrud('users');
+
+const users = ref([]);
+const error = ref(null);
+const loading = ref(true);
+
+onMounted(() => {
+  readAll()
+    .then(response => {
+      console.log('Données des utilisateurs reçues:', response);
+      users.value = response; // Assurez-vous que response contient les données utilisateur
+      loading.value = false;
+    })
+    .catch(err => {
+      console.error('Erreur lors de la récupération des utilisateurs:', err);
+      error.value = err;
+      loading.value = false;
+    });
+});
+</script>
